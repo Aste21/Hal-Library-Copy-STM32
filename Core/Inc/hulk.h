b778e0feed612e06485982c260ec27c908bf89fa
Wiktor Kopczyński <kopczynski.wkt@gmail.com>
2024-05-21 15:29:02 +0200

    Add files via upload

--- Core/Inc/hulk.h
+++ Core/Inc/hulk.h
@@ -10,6 +10,8 @@
 
 #include <stdint.h>
 #include "stm32l4xx_hal.h"
+#include <stdio.h>
+#include <ctype.h>
 
 typedef uint16_t pin_t;
 typedef struct
@@ -27,25 +29,63 @@ typedef struct
 	volatile uint32_t PWR_CR2_tt;
 }PWR_CR2_t;
 
+#define MODE_BIT_LENGTH				2
 #define MODE_INPUT_m				0x00000000u
 #define MODE_OUTPUT_m 				0x00000001u
-#define MODE_ALTENATE_FUNCTION_m 	0x00000002u
+#define MODE_ALTERNATE_FUNCTION_m 	0x00000002u
 #define MODE_ANALOG_m				0x00000003u
 
+#define TYPE_BIT_LENGTH				1
 #define TYPE_PUSH_PULL				0x00000000u
 #define TYPE_OPEN_DRAIN				0x00000001u
 
+#define SPEED_BIT_LENGTH			2
 #define SPEED_LOW					0x00000000u
 #define SPEED_MEDIUM				0x00000001u
 #define SPEED_HIGH					0x00000002u
 #define SPEED_VERY_HIGH				0x00000003u
 
+#define PULL_BIT_LENGTH				2
 #define NO_PULL						0x00000000u
 #define PULL_UP						0x00000001u
 #define PULL_DOWN					0x00000002u
 
+
+#define DATA_BITS_8					0x00000000u
+#define DATA_BITS_9					0x00000001u
+#define DATA_BITS_7					0x00000002u
+
+#define STOP_BIT_LENGTH				2
+#define STOP_BITS_1					0x00000000u
+#define STOP_BITS_2					0x00000002u
+#define STOP_BITS_RESET				0x00000000u
+
+#define DMAT_BIT_LENGTH				1
+#define DISABLE_DMAT				0x00000000u
+#define ENABLE_DMAT					0x00000001u
+
+#define DMAR_BIT_LENGTH				1
+#define DISABLE_DMAR				0x00000000u
+#define ENABLE_DMAR					0x00000001u
+
+#define RE_BIT_LENGTH				1
+#define DISABLE_RECEIVER			0x00000000u
+#define ENABLE_RECEIVER				0x00000001u
+
+#define TE_BIT_LENGTH				1
+#define DISABLE_TRANSMITTER			0x00000000u
+#define ENABLE_TRANSMITTER			0x00000001u
+
+#define UE_BIT_LENGTH				1
+#define DISABLE_LPUART				0x00000000u
+#define ENABLE_LPUART				0x00000001u
+
+#define CLASSIC_BRR					115200
+
 #define DISABLE_STATE				0x00000000u
 #define ENABLE_STATE				0x00000001u
+
+#define AF_8						0x00000008u
 
 
 
@@ -77,36 +117,79 @@ typedef struct
 
 #define TIM6_m						((TIMx_t*)0x40001000)
 
+#define LPUART_m					((LPUART_t*)0x40008000)
+
 #define RCC_m						(0x40021000)
 
 #define AHB2_OFFSET					(0x4C)
 #define AHB2						((AHB2_t*)(RCC_m+AHB2_OFFSET))
 
-#define APB1_OFFSET 				(0x58)
-#define APB1						((APB1_t*)(RCC_m+APB1_OFFSET))
+#define APB1ENR1_OFFSET 			(0x58)
+#define APB1ENR1_m					((APB1_t*)(RCC_m+APB1ENR1_OFFSET))
+
+#define APB1ENR2_OFFSET 			(0x5C)
+#define APB1ENR2_m					((APB1_t*)(RCC_m+APB1ENR2_OFFSET))
+
 
 #define PWR_m						(0x40007000)
 #define PWR_CR2_OFFSET				(0x04)
 #define PWR_CR2						((PWR_CR2_t*)(PWR_m+PWR_CR2_OFFSET))
 
+#define RSTVALCALC(N)				(((1<<N)-1))
+
 #define BIT(x)						(1U << (x))
 #define SETBIT(P, B)				((P) |= BIT(B))
 #define CLRBIT(P, B)				((P) &= ~BIT(B))
+#define RSTSETTING(P, B, N)			((P) &= ~(RSTVALCALC(N) << B))
+#define SETSETTING(P, B, S)			((P) |= ((S) << (B)))
+
+#define CHANGESETTING(P, B, N, S) \
+    do { \
+        RSTSETTING(P, B, N); \
+        SETSETTING(P, B, S); \
+    } while (0)
+	//P is the register, B is the bit number in register, N is the number of bits of setting, S is the setting
 
 #define CLK_ENABLE(x)				SETBIT(AHB2->AHB2_tt, x)
 #define EnableVddIO2()				SETBIT(PWR_CR2->PWR_CR2_tt, 9)
 
 #define TIM6_BIT					4
-#define TIM6_ENABLE()				SETBIT(APB1->APB1_tt, TIM6_BIT)
+#define TIM6_ENABLE()				SETBIT(APB1ENR1_m->APB1_tt, TIM6_BIT)
 
+#define LPUART_BIT					0
+#define LPUART_ENABLE_CLK()			SETBIT(APB1ENR2_m->APB1_tt, LPUART_BIT)
+
+#define CEN_BIT_LENGTH				1
 #define CEN_BIT						0
+#define ARPE_BIT_LENGTH				1
 #define ARPE_BIT					7
+#define OPM_BIT_LENGTH				1
 #define OPM_BIT						3
+#define URS_BIT_LENGTH				1
 #define URS_BIT						2
+#define UDIS_BIT_LENGTH				1
 #define UDIS_BIT					1
+#define UG_BIT_LENGTH				1
 #define UG_BIT						0
+#define UIF_BIT_LENGTH				1
 #define UIF_BIT						0
+#define UIE_BIT_LENGTH				1
 #define UIE_BIT						0
+
+#define M0_BIT						12
+#define M1_BIT						28
+#define STOP_BIT					12
+#define UE_BIT						0
+#define DMAT_BIT					7
+#define DMAR_BIT					6
+#define RE_BIT						2
+#define TE_BIT						3
+#define TXE_BIT						7
+#define TC_BIT						6
+#define RXNE_BIT					5
+
+#define CLASSIC_BRR_VALUE			((256 * 4000000) / 115200)
+#define CLEAR_SCREEN_LINES			100
 
 typedef enum
 {
@@ -165,18 +248,25 @@ typedef struct
 
 typedef struct
 {
-	volatile uint32_t CR1;
-	volatile uint32_t CR2;
+	volatile uint16_t CR1;
+	volatile uint16_t CR1_RESERVED;
+	volatile uint16_t CR2;
+	volatile uint16_t CR2_RESERVED;
 	volatile uint32_t RESERVED1;
-	volatile uint32_t DIER;
-	volatile uint32_t SR;
-	volatile uint32_t EGR;
+	volatile uint16_t DIER;
+	volatile uint16_t DIER_RESERVED;
+	volatile uint16_t SR;
+	volatile uint16_t SR_RESERVED;
+	volatile uint16_t EGR;
+	volatile uint16_t EGR_RESERVED;
 	volatile uint32_t RESERVED2;
 	volatile uint32_t RESERVED3;
 	volatile uint32_t RESERVED4;
 	volatile uint32_t CNT;
-	volatile uint32_t PSC;
-	volatile uint32_t ARR;
+	volatile uint16_t PSC;
+	volatile uint16_t PSC_RESERVED;
+	volatile uint16_t ARR;
+	volatile uint16_t ARR_RESERVED;
 } TIMx_t;
 
 typedef struct
@@ -200,11 +290,48 @@ typedef struct
 	uint16_t PERIOD;
 } TIMInit_t;
 
+typedef struct
+{
+	volatile uint32_t CR1;
+	volatile uint32_t CR2;
+	volatile uint32_t CR3;
+	volatile uint32_t BRR;
+	volatile uint32_t RESERVED1;
+	volatile uint32_t RESERVED2;
+	volatile uint16_t RQR;
+	volatile uint16_t RQR_RESERVED;
+	volatile uint32_t ISR;
+	volatile uint32_t ICR;
+	volatile uint16_t RDR;
+	volatile uint16_t RDR_RESERVED;
+	volatile uint16_t TDR;
+	volatile uint16_t TDR_RESERVED;
+} LPUART_t;
+
+typedef struct
+{
+	uint32_t Word_Length;
+
+	uint32_t Stop_Bits;
+
+	uint32_t DMAT;
+
+	uint32_t DMAR;
+
+	uint32_t BRR;
+
+	uint32_t RE;
+
+	uint32_t TE;
+
+	uint32_t UE;
+} LPUARTInit_t;
+
 void GPIO_init(GPIOx_t *GPIOx, GPIOInit_t *GPIO_Init);
 void TIM_init(TIMx_t *TIMx, TIMInit_t *TIM_Init);
+void LPUART_init(LPUART_t *LPUART, LPUARTInit_t *LPUART_Init);
 void turn_on_pin(GPIOx_t* GPIOx, pin_t GPIO_Pin);
 void turn_off_pin(GPIOx_t* GPIOx, pin_t GPIO_Pin);
-void HULK_delay_old(int time);
 void HULK_delay(int miliseconds);
 PinState read_pin(GPIOx_t* GPIOx, pin_t GPIO_Pin);
 FlagState check_flag(TIMx_t* TIMx);
@@ -212,5 +339,14 @@ void clear_flag(TIMx_t* TIMx);
 void basic_TIM6_init();
 void start_timer();
 void stop_timer();
+void basic_LPUART_init();
+void LPUART_SendChar(const char data);
+char LPUART_ReadChar();
+void LPUART_SendString(char* data);
+void LPUART_NewLine();
+char toggle_case_char(char c);
+void LPUART_ClearScreen();
+
+
 
 #endif /* INC_HULK_H_ */
